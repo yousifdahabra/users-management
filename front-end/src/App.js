c78c394d81995ba75059fcf66935770c8ef9edd7
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-02 02:31:01 +0200

    Front-end - add  react-router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,15 @@
 import './styles/App.css';
-
+import {Router,Routes,BrowserRouter} from'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+          <Router path='/' elemment={<></>}/>
+        </Routes>
+      
+      </BrowserRouter>
     
     </div>
   );
